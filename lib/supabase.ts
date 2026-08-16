5982fee948582a1ffa22594d27eb87bec52abe91
vrodeprogramist <54kos_sh@mail.ru>
2026-08-16 20:20:24 +0700

    Добавил ключи в код

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// ВСТАВЬ СВОИ ДАННЫЕ (возьми их из Supabase)
+const supabaseUrl = 'https://aqfnuyupkvznihzwhpqu.supabase.co'; // твой URL
+const supabaseAnonKey = 'sb_publishable_uIGZ_TBmUa0E_6zEAlfErQ_Ce2QM9ak'; // твой ключ
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
